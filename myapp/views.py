c0603e0709b0ebc227e18131af4a2f593c66c8e0
Gaurav Madan <gauravmadan2004@gmail.com>
2023-01-27 20:45:51 +0530

    initial commit

---
+++ myapp/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def index(request):
+    context = {
+        'name': 'Gaurav',
+        'age' : 18,
+        'nationality':'Indian'
+    }
+    return render( request, 'index.html', context)
+
+def counter(request):
+    text = request.POST['text']
+    amount_of_words = len(text.split())
+    return render(request, 'counter.html',{'amount': amount_of_words})
